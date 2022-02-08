ba1fff518ed2200dbe36136d3e52b170df905110
Christopher Meis <christopher.meis@9elements.com>
2022-02-08 13:19:29 +0100

    Rest-API Setup works locally
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- createDBscheme.sql
+++ createDBscheme.sql
@@ -1,7 +1,9 @@
+\c internet_of_dinge;
+
 CREATE SCHEMA IF NOT EXISTS iotproject;
 
 CREATE TABLE iotproject.soilmoisture(
-    uid INTEGER NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     hardware_flag VARCHAR(1),
     interrupt_flag VARCHAR(1),
     sensor_flag VARCHAR(1),
@@ -13,7 +15,7 @@ CREATE TABLE iotproject.soilmoisture(
 );
 
 CREATE TABLE iotproject.gajewskisensor(
-    uid INTEGER NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     css811_eco2 REAL,
     css811_etvoc REAL,
     mq9_ac REAL,
