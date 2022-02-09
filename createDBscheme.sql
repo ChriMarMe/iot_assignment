eb6c8fd14b3ae122c6e82685e3e2de814603fa4f
Christopher Meis <christopher.meis@9elements.com>
2022-02-09 11:43:04 +0100

    Some more adjustments
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- createDBscheme.sql
+++ createDBscheme.sql
@@ -4,11 +4,12 @@ CREATE SCHEMA IF NOT EXISTS iotproject;
 
 CREATE TABLE iotproject.soilmoisture(
     id SERIAL PRIMARY KEY,
+    add_time TIMESTAMP DEFAULT NOW(),
+    batterie REAL,
     hardware_flag REAL,
     interrupt_flag REAL,
     sensor_flag REAL,
     tempc_ds18b20 REAL,
-    batterie REAL,
     conduct_soil REAL,
     temp_soil REAL,
     water_soil REAL
@@ -16,6 +17,7 @@ CREATE TABLE iotproject.soilmoisture(
 
 CREATE TABLE iotproject.gajewskisensor(
     id SERIAL PRIMARY KEY,
+    add_time TIMESTAMP DEFAULT NOW(),
     css811_eco2 REAL,
     css811_etvoc REAL,
     mq9_ac REAL,
